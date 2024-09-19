1f96d65ce2f09c7a6429c4bcdde0c89affd3d17c
mnaatjes <michael.naatjes87@gmail.com>
2024-09-19 11:06:59 -0400

    Project Finished

--- scripts/js/functions/function_prompt.js
+++ scripts/js/functions/function_prompt.js
@@ -1,7 +1,46 @@
 //-------------------------------------------------------------------------------------------------------
 // CIS 174: Functions - Prompt
 //-------------------------------------------------------------------------------------------------------
+/*------------------------------------------------------*/
+/***
+ * @name validateInput
+ * @type {function}
+ */
+/*------------------------------------------------------*/
+function validateInput(input){
+    const alert = document.querySelector('small');
+    if(isNaN(input)){
+        alert.style.display = 'block';
+        return false;
+    } else {
+        alert.style.display = 'none';
+        return true;
+    }
+}
+/*------------------------------------------------------*/
+/***
+ * @name drawNumber
+ * @type {function}
+ */
+/*------------------------------------------------------*/
+function drawNumber(element, number, type=""){
+    const cube  = element.querySelector('[class="cube"]');
+    const print = element.querySelector('p');
 
+    // do maths
+    if(type == 'root'){
+        number = Math.cbrt(number).toFixed(2);
+    } else if (type == 'cube'){
+        number = Math.pow(number, 3).toFixed(2);
+        number = Number.parseFloat(number).toExponential(2);
+    }
+    // print to screen
+    print.innerHTML = number;
+    // loop faces of cubes
+    for(let i = 0; i < cube.children.length; i++){
+        cube.children[i].innerHTML = number;
+    }
+}
 /*------------------------------------------------------*/
 /***
  * @name promptNumber
@@ -9,5 +48,18 @@
  */
 /*------------------------------------------------------*/
 function promptNumber(){
-    
+    // define prompt
+    const prompt_num = prompt('Please enter a whole number.', 2);
+    // validate
+    if(validateInput(prompt_num) == true){
+        // define elements
+        const root      = document.getElementById('root');
+        const number    = document.getElementById('number');
+        const cubed     = document.getElementById('cubed');
+
+        // draw output
+        drawNumber(root, prompt_num, 'root');
+        drawNumber(number, prompt_num);
+        drawNumber(cubed, prompt_num, 'cube');
+    }
 }
